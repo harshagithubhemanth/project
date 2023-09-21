36d1bb32741dc7e531b25a513304a08dee9796ba
harshagithubhemanth <145647328+harshagithubhemanth@users.noreply.github.com>
2023-09-21 21:55:31 +0530

    Update save.php

--- food_blog/food_blog/save.php
+++ food_blog/food_blog/save.php
@@ -6,6 +6,6 @@ $message = $_POST["message"];
 $conn = mysqli_connect("localhost" , "root" , "" , "contact_form") or die("connection failed");
 $sql = "INSERT INTO contact_table(fname , email , subject , message) VALUES ('{$fname}' , '{$email}' , '{$subject}' , '{$message}')";
 $result = mysqli_query($conn, $sql) or die("Query Failed");
-header("location: http://localhost/food_blog/contact.php");
+header("location: https://localhost/food_blog/contact.php");
 mysqli_close($conn);
 ?>
